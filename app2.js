a881e8447e8b6288d5a6839c6335c2535223d54a
Charos12 <andrew.le12@gmail.com>
2020-04-03 22:12:34 -0400

    first thousand

--- app2.js
+++ app2.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
   // AJAX in the data file
   $.ajax({
     type: "GET",
-    url: "BTC_M15_Candle.csv",
+    url: "BTC_M15_Candle_1000.csv",
     dataType: "text",
     success: function(data) {
       processData(data);
